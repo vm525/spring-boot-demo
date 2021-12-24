a7598779e63719fd04ea4490680691096865e55e
Vibhor Mittal <vibhormittal007@gmail.com>
2021-12-24 10:47:51 +0530

    feat: update HTTP status code for saveDepartment end-point

--- src/main/java/com/nvm/springbootdemo/controller/DepartmentController.java
+++ src/main/java/com/nvm/springbootdemo/controller/DepartmentController.java
@@ -3,11 +3,8 @@ package com.nvm.springbootdemo.controller;
 import com.nvm.springbootdemo.entity.Department;
 import com.nvm.springbootdemo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DeadlockLoserDataAccessException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
@@ -18,6 +15,7 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @PostMapping("/department/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public Department saveDepartment(@RequestBody Department department) {
         // beauty of @RequestBody => automatically unmarshall and decodes JSON from client to Department java object
         return departmentService.saveDepartment(department);
